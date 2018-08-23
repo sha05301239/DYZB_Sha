9a25846ba8d303046d3dcca15f93ef22664d7e39
sha <136537419@qq.com>
2018-08-23 18:47:39 +0800

    完成tabbar的封装，正在做titleView

---
+++ DouYuZB/DouYuZB/Classes/Main/View/XZPageTitleView.swift
@@ -0,0 +1,21 @@
+//
+//  XZPageTitleView.swift
+//  DouYuZB
+//
+//  Created by 再出发 on 2018/8/23.
+//  Copyright © 2018年 sha xianding. All rights reserved.
+//
+
+import UIKit
+
+class XZPageTitleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DouYuZB/DouYuZB/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIBarButtonItem-Extension.swift
+//  DouYuZB
+//
+//  Created by 再出发 on 2018/8/23.
+//  Copyright © 2018年 sha xianding. All rights reserved.
+//
+
+import Foundation
